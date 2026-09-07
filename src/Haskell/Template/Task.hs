d7c2fcfc0873c5030e2c90d23e6aa2ce8bc124a1
nimec01 <24428341+nimec01@users.noreply.github.com>
2026-09-07 15:57:52 +0200

    in-line into Task.hs

--- src/Haskell/Template/Task.hs
+++ src/Haskell/Template/Task.hs
@@ -10,20 +10,31 @@
 {-# LANGUAGE TupleSections #-}
 {-# LANGUAGE NamedFieldPuns #-}
 module Haskell.Template.Task (
+  FSolutionConfig (..),
+  SolutionConfig,
+  HaskellConfig (..),
   check,
+  defaultCode,
+  defaultHaskellConfig,
+  defaultSolutionConfig,
+  displayHaskellConfig,
+  finaliseConfigs,
   getCodeWorldButtonOption,
   getCodeWorldRenderButtonOption,
   getCodeWorldPartialRenderButtonOption,
   getHlintFeedback,
   grade,
+  initialTask,
   matchTemplate,
   maybeSampleSolution,
   parse,
+  parseHaskellConfig,
   rejectHint,
   rejectMatch,
-  initialTask
+  toSolutionConfigOpt,
   ) where
 
+import qualified Data.ByteString.Char8            as BS
 import qualified Language.Haskell.Exts            as E
 import qualified Language.Haskell.Exts.Parser     as P
 import qualified System.IO                        as IO {- required to avoid encoding problems -}
@@ -34,13 +45,15 @@ import Haskell.Template.Match
   (Location (..), Result (..), What (..), Where (..), highlight_ssi)
 import qualified Haskell.Template.Match as Match (test)
 
+import Control.Applicative              ((<|>))
 import Control.Monad                    (forM, guard, msum, unless, void, when)
 import Control.Monad.Extra              ((&&^), whenJust)
 import Control.Monad.IO.Class           (MonadIO)
 import Data.Char                        (isUpper)
 import Data.Functor.Identity            (Identity (..))
+import Data.Either.Extra                   (fromRight')
 import Data.List
-  (delete, elemIndex, intercalate, isInfixOf,
+  (delete, elemIndex, groupBy, intercalate, intersperse, isInfixOf, isPrefixOf,
    union,
    )
 import Data.List.Extra
@@ -48,6 +61,11 @@ import Data.List.Extra
 import Data.Maybe                       (fromMaybe)
 import Data.Text.Lazy                   (pack)
 import Data.Typeable                    (Typeable)
+import Data.Yaml
+  (FromJSON, ParseException, ToJSON, decodeEither')
+import Data.Yaml.Pretty
+  (defConfig, encodePretty, setConfCompare)
+import GHC.Generics                     (Generic (..))
 import Language.Haskell.HLint           (hlint)
 import Language.Haskell.Interpreter
   (GhcError (..), InterpreterError (..), MonadInterpreter, OptionVal (..),
@@ -68,9 +86,309 @@ import Text.PrettyPrint.Leijen.Text
   (Doc, (<+>), empty, int, linebreak, nest, punctuate, text, vcat)
 import Text.Read                        (readMaybe)
 import Text.Regex.PCRE.Heavy            (re, sub)
-import Haskell.Template.Config (HaskellConfig (..), SolutionConfig, FSolutionConfig (..), FeedbackPhase (..))
 
+encode :: ToJSON a => a -> BS.ByteString
+encode = encodePretty $ setConfCompare compare defConfig
 
+defaultCode :: String
+defaultCode = BS.unpack (encode defaultSolutionConfig) ++
+  [SI.i|\#\#\#\#\# parameter description:
+\# allowAdding                 - allow adding program parts
+\# allowModifying              - allow modifying program parts
+\# allowRemoving               - allow removing program parts
+\# addCodeWorldButton          - adds a button to transfer student visible code
+\#                               into the CodeWorld editor
+\# addCodeWorldRenderButton    - adds a button to transfer student visible code
+\#                               into the CodeWorld runner
+\# addCodeWorldPartialRenderButton - adds a button to transfer student visible
+\#                                   code into the CodeWorld runner with
+\#                                   preview for code containing 'undefined'
+\# configGhcLimit              - caps amount of GHC warnings/errors to display
+\# configGhcErrors             - GHC warnings to enforce
+\# configGhcWarnings           - GHC warnings to provide as hints
+\# configHlintSuggestionsLimit - caps amount of hlint suggestions to display
+\# configHlintErrors           - hlint hints to enforce, only first one encountered is displayed
+\# configHlintGroups           - hlint extra hint groups to use
+\# configHlintRules            - hlint extra hint rules to use
+\# configHlintSuggestions      - hlint hints to provide as suggestions
+\# configLanguageExtensions    - this sets LanguageExtensions for hlint as well
+\# maxLineLength               - submissions with lines longer than this value are rejected
+\# syntaxCutoff                - determines the last step in the syntax phase (later steps are considered semantics);
+\#                               possible values (and also the order of steps):
+\#                                 CodeWidth, Compilation, GhcErrors, HlintErrors, TemplateMatch, TestSuite
+\#                               default on omission is TemplateMatch; steps after TestSuite are (in this order):
+\#                                 GhcWarnings, HlintSuggestions
+\# disableSemantics            - will prevent the semantics phase (as determined by syntaxCutoff) from running;
+\#                               this means a submission will be accepted after passing the syntax phase
+\# provideSampleSolution       - display provided sample solution to students after semantics feedback
+\# rigorousValidation          - will run all tests configured for submissions on the provided sample solution
+\#                               (no effect if there is none);
+\#                               this should be set while configuring the task and disabled after,
+\#                               in order to reduce wait times for students
+\# messageOnCloningSampleSolution - compare provided sample solution with submission and output
+\#                                  this message as feedback if the submission contains the sample solution
+\#                                  (provideSampleSolution will be ignored if the submission is a clone)
+----------
+module Solution where
+import Prelude
+
+r :: [a] -> [a]
+r = undefined
+
+----------
+{- You can add additional modules separated by lines of three or more dashes: -}
+{-\# LANGUAGE ScopedTypeVariables \#-}
+module Test (test) where
+import Prelude
+{-
+If this module is present, Test.test is used to check the submission.
+Otherwise, Solution.test is used.
+
+'test' has to be Test.HUnit.Testable, so assertions build with (@?=) will work,
+as do plain 'Bool's.
+If your test suite comprises more than a single assertion, you should use a list
+of named test cases (see (~:)) to provide better feedback.
+
+Example:
+-}
+import TestHelper (qc)
+import TestHarness
+import Test.HUnit (Test, (@?=), (~:))
+
+import qualified Solution
+
+test :: [Test]
+test =
+  ["Test with QuickCheck (random input)" ~:
+     qc 5000 $ \\(xs :: [Int]) ->
+       Solution.r xs == Prelude.reverse xs
+  ]
+----------
+module SampleSolution where
+import Prelude
+
+{-
+This module may provide a sample solution.
+Including it is currently optional, but strongly encouraged,
+as the sample will be validated the same way a student's submission would,
+thus preventing a broken configuration or impossible task.
+-}
+
+r :: [a] -> [a]
+r = reverse
+
+----------
+module SomeHiddenModule where
+import Prelude
+{- This module is also not shown to the student but is available to the code -}
+{-
+Also available are the following modules:
+
+  TestHelper   (Import this in Solution or Test)
+    (Use either of the following instead of 'quickCheck' to turn a property into a HUnit assertion.)
+    qcWithArgs :: Testable prop => Int -> Args -> prop -> Assertion
+      (Provide a timeout (in ms) and Arbitrary QuickCheck Args)
+    qc'        :: Testable prop => Int -> Int -> prop -> Assertion
+      (Provide a timeout (in ms) and a number for 'maxSuccess')
+    qc         :: Testable prop => Int -> prop -> Assertion
+      (Provide a timeout (in ms))
+
+  TestHarness  (Import this in Test)
+    syntaxCheck :: (Module SrcSpanInfo -> Assertion) -> Assertion
+    findTopLevelDeclsOf :: String -> Module SrcSpanInfo -> [Decl SrcSpanInfo]
+    contains
+    ident :: String -> Name SrcSpanInfo -> Bool
+      (Used to implement syntax checks. Example usage: see above)
+
+    allowFailures :: Int -> [Test] -> Assertion
+      (Detailed output of correct/incorrect Tests in case of failure,
+      with the option to allow a fixed number of tests to fail.)
+ -}|]
+
+data FeedbackPhase
+  = CodeWidth
+  | Compilation
+  | GhcErrors
+  | HlintErrors
+  | TemplateMatch
+  | TestSuite
+  deriving (Enum, Eq, Generic, Show, FromJSON, ToJSON)
+
+data FSolutionConfig m = SolutionConfig {
+    allowAdding                 :: m Bool,
+    allowModifying              :: m Bool,
+    allowRemoving               :: m Bool,
+    addCodeWorldButton          :: m Bool,
+    addCodeWorldRenderButton    :: m Bool,
+    addCodeWorldPartialRenderButton :: m Bool,
+    configGhcLimit              :: m (Maybe Natural),
+    configGhcErrors             :: m [String],
+    configGhcWarnings           :: m [String],
+    configHlintSuggestionsLimit :: m (Maybe Natural),
+    configHlintErrors           :: m [String],
+    configHlintGroups           :: m [String],
+    configHlintRules            :: m [String],
+    configHlintSuggestions      :: m [String],
+    configLanguageExtensions    :: m [String],
+    maxLineLength               :: m (Maybe Natural),
+    provideSampleSolution       :: m Bool,
+    messageOnCloningSampleSolution :: m (Maybe String),
+    disableSemantics            :: m Bool,
+    rigorousValidation          :: m Bool,
+    syntaxCutoff                :: m FeedbackPhase
+  } deriving Generic
+
+type SolutionConfigOpt = FSolutionConfig Maybe
+
+deriving instance Show SolutionConfigOpt
+deriving instance FromJSON SolutionConfigOpt
+deriving instance ToJSON SolutionConfigOpt
+
+type SolutionConfig  = FSolutionConfig Identity
+
+deriving instance Show SolutionConfig
+deriving instance FromJSON SolutionConfig
+deriving instance ToJSON SolutionConfig
+
+data HaskellConfig = HaskellConfig
+  { solutionConfig :: SolutionConfig
+  , modules :: [String]
+  } deriving Show
+
+defaultSolutionConfig :: SolutionConfigOpt
+defaultSolutionConfig = SolutionConfig {
+    allowAdding                 = Just True,
+    allowModifying              = Just False,
+    allowRemoving               = Just False,
+    addCodeWorldButton          = Just True,
+    addCodeWorldRenderButton    = Just True,
+    addCodeWorldPartialRenderButton = Just False,
+    configGhcLimit              = Just Nothing,
+    configGhcErrors             = Just [],
+    configGhcWarnings           = Just [],
+    configHlintSuggestionsLimit = Just Nothing,
+    configHlintErrors           = Just [],
+    configHlintGroups           = Just [],
+    configHlintRules            = Just [],
+    configHlintSuggestions      = Just [],
+    configLanguageExtensions    = Just ["NPlusKPatterns","ScopedTypeVariables"],
+    maxLineLength               = Just Nothing,
+    provideSampleSolution       = Just False,
+    messageOnCloningSampleSolution = Just Nothing,
+    disableSemantics            = Just False,
+    rigorousValidation          = Just False,
+    syntaxCutoff                = Just TemplateMatch
+  }
+
+toSolutionConfigOpt :: SolutionConfig -> SolutionConfigOpt
+toSolutionConfigOpt SolutionConfig {..} = runIdentity $ SolutionConfig
+  <$> fmap Just allowAdding
+  <*> fmap Just allowModifying
+  <*> fmap Just allowRemoving
+  <*> fmap Just addCodeWorldButton
+  <*> fmap Just addCodeWorldRenderButton
+  <*> fmap Just addCodeWorldPartialRenderButton
+  <*> fmap Just configGhcLimit
+  <*> fmap Just configGhcErrors
+  <*> fmap Just configGhcWarnings
+  <*> fmap Just configHlintSuggestionsLimit
+  <*> fmap Just configHlintErrors
+  <*> fmap Just configHlintGroups
+  <*> fmap Just configHlintRules
+  <*> fmap Just configHlintSuggestions
+  <*> fmap Just configLanguageExtensions
+  <*> fmap Just maxLineLength
+  <*> fmap Just provideSampleSolution
+  <*> fmap Just messageOnCloningSampleSolution
+  <*> fmap Just disableSemantics
+  <*> fmap Just rigorousValidation
+  <*> fmap Just syntaxCutoff
+
+finaliseConfigs :: [SolutionConfigOpt] -> Maybe SolutionConfig
+finaliseConfigs = finaliseConfig . foldl combineConfigs emptyConfig
+  where
+    finaliseConfig :: SolutionConfigOpt -> Maybe SolutionConfig
+    finaliseConfig SolutionConfig {..} = SolutionConfig
+      <$> fmap Identity allowAdding
+      <*> fmap Identity allowModifying
+      <*> fmap Identity allowRemoving
+      <*> fmap Identity addCodeWorldButton
+      <*> fmap Identity addCodeWorldRenderButton
+      <*> fmap Identity addCodeWorldPartialRenderButton
+      <*> fmap Identity configGhcLimit
+      <*> fmap Identity configGhcErrors
+      <*> fmap Identity configGhcWarnings
+      <*> fmap Identity configHlintSuggestionsLimit
+      <*> fmap Identity configHlintErrors
+      <*> fmap Identity configHlintGroups
+      <*> fmap Identity configHlintRules
+      <*> fmap Identity configHlintSuggestions
+      <*> fmap Identity configLanguageExtensions
+      <*> fmap Identity maxLineLength
+      <*> fmap Identity provideSampleSolution
+      <*> fmap Identity messageOnCloningSampleSolution
+      <*> fmap Identity disableSemantics
+      <*> fmap Identity rigorousValidation
+      <*> fmap Identity syntaxCutoff
+    combineConfigs x y = SolutionConfig {
+        allowAdding                 = allowAdding                 x <|> allowAdding                 y,
+        allowModifying              = allowModifying              x <|> allowModifying              y,
+        allowRemoving               = allowRemoving               x <|> allowRemoving               y,
+        addCodeWorldButton          = addCodeWorldButton          x <|> addCodeWorldButton          y,
+        addCodeWorldRenderButton    = addCodeWorldRenderButton    x <|> addCodeWorldRenderButton    y,
+        addCodeWorldPartialRenderButton = addCodeWorldPartialRenderButton x <|> addCodeWorldPartialRenderButton y,
+        configGhcLimit              = configGhcLimit              x <|> configGhcLimit              y,
+        configGhcErrors             = configGhcErrors             x <|> configGhcErrors             y,
+        configGhcWarnings           = configGhcWarnings           x <|> configGhcWarnings           y,
+        configHlintSuggestionsLimit = configHlintSuggestionsLimit x <|> configHlintSuggestionsLimit y,
+        configHlintErrors           = configHlintErrors           x <|> configHlintErrors           y,
+        configHlintGroups           = configHlintGroups           x <|> configHlintGroups           y,
+        configHlintRules            = configHlintRules            x <|> configHlintRules            y,
+        configHlintSuggestions      = configHlintSuggestions      x <|> configHlintSuggestions      y,
+        configLanguageExtensions    = configLanguageExtensions    x <|> configLanguageExtensions    y,
+        maxLineLength               = maxLineLength               x <|> maxLineLength               y,
+        provideSampleSolution       = provideSampleSolution       x <|> provideSampleSolution       y,
+        messageOnCloningSampleSolution = messageOnCloningSampleSolution x <|> messageOnCloningSampleSolution y,
+        disableSemantics            = disableSemantics            x <|> disableSemantics            y,
+        rigorousValidation          = rigorousValidation          x <|> rigorousValidation          y,
+        syntaxCutoff                = syntaxCutoff                x <|> syntaxCutoff                y
+      }
+    emptyConfig = SolutionConfig {
+        allowAdding                 = Nothing,
+        allowRemoving               = Nothing,
+        allowModifying              = Nothing,
+        addCodeWorldButton          = Nothing,
+        addCodeWorldRenderButton    = Nothing,
+        addCodeWorldPartialRenderButton = Nothing,
+        configGhcLimit              = Nothing,
+        configGhcErrors             = Nothing,
+        configGhcWarnings           = Nothing,
+        configHlintSuggestionsLimit = Nothing,
+        configHlintErrors           = Nothing,
+        configHlintGroups           = Nothing,
+        configHlintRules            = Nothing,
+        configHlintSuggestions      = Nothing,
+        configLanguageExtensions    = Nothing,
+        maxLineLength               = Nothing,
+        provideSampleSolution       = Nothing,
+        messageOnCloningSampleSolution = Nothing,
+        disableSemantics            = Nothing,
+        rigorousValidation          = Nothing,
+        syntaxCutoff                = Nothing
+      }
+
+displayHaskellConfig :: HaskellConfig -> String
+displayHaskellConfig HaskellConfig{..} = unlines $
+  BS.unpack (encode solutionConfig) : if null modules then [] else "----------" : intersperse "----------" modules
+
+parseHaskellConfig :: String -> Either Doc HaskellConfig
+parseHaskellConfig raw = do
+  (solConfig, modules') <- splitConfigAndModules Left raw
+  completedConfig <- addDefaults Left solConfig
+  return $ HaskellConfig
+    { solutionConfig = completedConfig
+    , modules = modules'
+    }
 
 string :: String -> Doc
 string = text . pack
@@ -121,7 +439,6 @@ check reject inform path HaskellConfig{solutionConfig = config@SolutionConfig{..
 initialTask :: HaskellConfig -> String
 initialTask HaskellConfig {..} = either id id $ do
   snd . fst <$> nameModules Left (extensionsOf solutionConfig) modules
-
 
 {- |
 Extract the sample solution if one was provided, 'provideSampleSolution' is enabled
@@ -514,6 +831,38 @@ bloc codeLines =
   let dash = string $ '+' : replicate 30 '-'
   in  vcat [ dash, vcat $ map (string . ("| " ++)) codeLines, dash ]
 
+splitConfigAndModules
+  :: Monad m
+  => (forall a. Doc -> m a)
+  -> String -> m (SolutionConfigOpt, [String])
+splitConfigAndModules reject configAndModules =
+  either (reject . string . ("Error while parsing config:\n" <>) . show)
+         (return . (,rawModules))
+         eConfig
+  where
+    configJson:rawModules = splitModules False configAndModules
+    eConfig :: Either ParseException SolutionConfigOpt
+    eConfig = decodeEither' $ BS.pack configJson
+
+addDefaults :: Monad m => (forall a. Doc -> m a) -> SolutionConfigOpt -> m SolutionConfig
+addDefaults reject f = maybe
+  (reject "There is a required configuration parameter missing")
+  return
+  $ finaliseConfigs [f, defaultSolutionConfig]
+
+splitModules :: Bool -> String -> [String]
+splitModules dropFirst = map unlines
+  . (if dropFirst then drop 1 else id)
+  . splitBy (isPrefixOf "---")
+  . lines
+
+splitBy :: (t -> Bool) -> [t] -> [[t]]
+splitBy p = dropOdd . groupBy (\l r -> not (p l) && not (p r))
+  where
+   dropOdd [] = []
+   dropOdd [x] = [x]
+   dropOdd (x:_:xs) = x:dropOdd xs
+
 nameModules
   :: Monad m
   => (forall a. String -> m a)
@@ -667,3 +1016,6 @@ checkLineLength reject code maxLength = case hasLonger of
       ]
     separated = vcat . punctuate linebreak
     rejectWithHint = rejectWithMessage reject rejectHint
+
+defaultHaskellConfig :: HaskellConfig
+defaultHaskellConfig = fromRight' $ parseHaskellConfig defaultCode
