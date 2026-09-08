3d011a38631038ec33b74cb58ec99db74f57a56f
nimec01 <24428341+nimec01@users.noreply.github.com>
2026-09-08 09:29:13 +0000

    Revert "directly use fromJust in addDefaults"
    This reverts commit 46de2ce2853add9e5398a3a65e8b4945ff902413.

--- src/Haskell/Template/Task.hs
+++ src/Haskell/Template/Task.hs
@@ -58,7 +58,7 @@ import Data.List
    )
 import Data.List.Extra
   (genericTake, nubOrd, replace, takeEnd, takeWhileEnd)
-import Data.Maybe                       (fromMaybe, fromJust)
+import Data.Maybe                       (fromMaybe)
 import Data.Text.Lazy                   (pack)
 import Data.Typeable                    (Typeable)
 import Data.Yaml
@@ -384,7 +384,7 @@ displayHaskellConfig HaskellConfig{..} = unlines $
 parseHaskellConfig :: String -> Either Doc HaskellConfig
 parseHaskellConfig raw = do
   (solConfig, modules') <- splitConfigAndModules Left raw
-  completedConfig <- addDefaults solConfig
+  completedConfig <- addDefaults Left solConfig
   return $ HaskellConfig
     { solutionConfig = completedConfig
     , modules = modules'
@@ -844,8 +844,11 @@ splitConfigAndModules reject configAndModules =
     eConfig :: Either ParseException SolutionConfigOpt
     eConfig = decodeEither' $ BS.pack configJson
 
-addDefaults :: Monad m => SolutionConfigOpt -> m SolutionConfig
-addDefaults f = return $ fromJust $ finaliseConfigs [f, toSolutionConfigOpt defaultSolutionConfig]
+addDefaults :: Monad m => (forall a. Doc -> m a) -> SolutionConfigOpt -> m SolutionConfig
+addDefaults reject f = maybe
+  (reject "There is a required configuration parameter missing")
+  return
+  $ finaliseConfigs [f, toSolutionConfigOpt defaultSolutionConfig]
 
 splitModules :: Bool -> String -> [String]
 splitModules dropFirst = map unlines
